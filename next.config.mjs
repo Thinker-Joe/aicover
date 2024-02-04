ab464559baa87659736a64a154c02417b6b9826a
tb-liangjingkun <tb_liangjingkun@jd.com>
2024-02-04 19:08:01 +0800

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,19 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   images: {
     domains: [
       "oaidalleapiprodscus.blob.core.windows.net",
       "trysai.s3.us-west-1.amazonaws.com",
     ],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, './'),
+    };
+    return config;
   },
 };
 
